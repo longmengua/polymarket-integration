23d7f1eb85c38196bb6a103b880ef17f0e0d3598
Waltor <longmengua@users.noreply.github.com>
2026-05-06 15:07:52 +0800

    update

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 docker rm -f polymarket-bot 2>/dev/null || true \
 && docker build -t polymarket-bot . \
-&& docker run -d --name polymarket-bot -p 80:3000 --restart=always polymarket-bot
+&& docker run -d --name polymarket-bot -p 443:3000 --restart=always polymarket-bot
